d6adce7c48040bb3bf8858f9551b2c8427e1856f
Mary Becica <mary.becica@appdirect.com>
2012-04-23 18:16:37 -0700

    interactions

--- js/site.js
+++ js/site.js
@@ -1,13 +1,54 @@
 $(function() {
-		//Resume
-		var resumeEl = $('#resume-template').html();
-		var resumeTemplate = Handlebars.compile(resumeEl);
-		$.ajax({
-			url:"js/resume.json",
-			success:function(data) {
-				$.each(data["resume"], function(i, d) {
-					$('#resume').append(resumeTemplate(d));
-				});
-			}
+		$('.splash').height($(window).height() - $('header').outerHeight());	
+		$('header').find('ul a').click(function() {
+			splashOn(false, $(this));
 		});
+		$('header').find('#home').click(function() {
+			splashOn(true);
+		});
+
+		var splashOn = function(mode, link) {
+			if (mode) {
+				$('#main').hide();
+				$('body').addClass('splashon');
+				$('header').find('a').removeClass('active');
+				$('.splash').css({'top':0}).show();
+			} else {
+				if ($('body').hasClass('splashon')) {
+					var oTop = $('header').offset().top;
+					$('.splash').animate({'top':-oTop}, 800, function() {
+						$('.splash').hide();
+					});
+					$('header').animate({'top':-oTop}, 800, 
+						function() {
+							$(this).css({'top':'0px'});
+							$('body').removeClass('splashon');
+							$('#main').show();
+				  });
+				};
+				$('header').find('a').removeClass('active');
+				link.addClass('active');
+				if (link.attr('id') == 'resumelink') {
+					//load resume
+					$('#resume').show();
+					makeResume();
+				} else if (link.attr('id') == 'worklink') {
+					//load Work
+					$('#resume').hide();
+				}
+			}
+		}
+		var makeResume = function() {
+			//Resume
+			var resumeEl = $('#resume-template').html();
+			var resumeTemplate = Handlebars.compile(resumeEl);
+			$.ajax({
+				url:"js/resume.json",
+				success:function(data) {
+					$.each(data["resume"], function(i, d) {
+						$('#resume').append(resumeTemplate(d));
+					});
+				}
+			});
+		}
 });
