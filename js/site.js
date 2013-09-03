34a0901b79ee82f43d25a886857ae4a4b6c06e01
mbecica <m.becica@gmail.com>
2013-09-03 00:18:03 -0700

    simplifying everything

--- js/site.js
+++ js/site.js
@@ -1,53 +1,38 @@
 $(function() {
-		$('.splash').height($(window).height() - $('header').outerHeight());	
-		$('header').find('ul a').click(function() {
-			splashOn(false, $(this));
+		
+    $('header').find('ul a').click(function() {
+			splashOff();
 		});
 		$('header').find('#home').click(function() {
-			splashOn(true);
+			splashOn();
 		});
 
-		var resumeLoad = false,
-				workLoad = false;
+		var resumeLoad = false;
 
-		var splashOn = function(mode, link) {
-			if (mode) {
-				$('#main').hide();
-				$('body').addClass('splashon');
-				$('header').find('a').removeClass('active');
-				$('.splash').css({'top':0}).show();
-			} else {
-				if ($('body').hasClass('splashon')) {
-					var oTop = $('header').offset().top;
-					$('.splash').animate({'top':-oTop}, 800, function() {
-						$('.splash').hide();
-					});
-					$('header').animate({'top':-oTop}, 800, 
-						function() {
-							$(this).css({'top':'0px'});
-							$('body').removeClass('splashon');
-							$('#main').show();
-				  });
-				};
-				$('header').find('a').removeClass('active');
-				link.addClass('active');
-				if (link.attr('id') == 'resumelink') {
-					//load resume
-					$('#resume').show();
-					$('#work').hide();
-					if (!resumeLoad) {
-						makeResume();
-					}
-				} else if (link.attr('id') == 'worklink') {
-					//load Work
-					$('#resume').hide();
-					$('#work').show();
-					if (!workLoad) {
-						makeWork();
-					}
-				}
-			}
+		var splashOn = function() {
+      $('#main').hide();
+      $('body').addClass('splashon');
+      $('header').css({'position':'fixed', 'top':'auto'});
+      imgGen();
 		}
+    var splashOff = function() {
+      var oTop = $(window).height() - $('header').outerHeight();
+      $('.splash').css('background-image', 'none');
+      $('header').css({'position':'relative', 'top':oTop}).animate({'top':0}, 800, 
+        function() {
+          $('body').removeClass('splashon');
+          $('#main').show();
+      });
+      //load resume
+      $('#resume').show();
+      if (!resumeLoad) {
+        makeResume();
+      }
+    }
+    var imgGen = function() {
+      //Generate background image here
+      $('.splash').css('background-image', "url(../img/turner.jpg)");
+    }
 		var makeResume = function() {
 			//Resume
 			var resumeEl = $('#resume-template').html();
@@ -62,26 +47,5 @@ $(function() {
 				}
 			});
 		}
-		var makeWork = function() {
-			//Work
-			var workEl = $('#work-template').html();
-			var workTemplate = Handlebars.compile(workEl);
-			$.ajax({
-				url:"js/work.json",
-				success:function(data) {
-					$.each(data["work"], function(i, d) {
-						$('#work').append(workTemplate(d));
-					});
-					workLoad = true;
-				}
-			});
-		}
+    imgGen();
 });
-
-var launch = function(href) {
-	window.open(href);
-}
-
-var show = function(images) {
-	//Nothing yet
-}
